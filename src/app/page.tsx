b7b6fe4f4119557993b816537d187303b444a925
Vitor Batista <vitiilimaa@gmail.com>
2025-02-16 15:02:46 -0300

    feat: criando projeto, instalando pacotes (chakra-ui, i18next, react-icons), configurando tema e testando componentes do chakra-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { ColorModeButton } from "@portfolio/components/ui/color-mode";
+import { MdNorthEast } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box>
+      <Flex gap={4} alignItems={"center"}>
+        <Button
+          fontWeight={"bold"}
+          background={"primary"}
+          fontSize={18}
+          size={"lg"}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Visualizar CV <MdNorthEast size={16} />
+        </Button>
+        <Button
+          fontWeight={"bold"}
+          fontSize={18}
+          variant={"outline"}
+          color={"primary"}
+          borderColor={"primary"}
+          size={"lg"}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          PT-BR
+        </Button>
+        <ColorModeButton />
+      </Flex>
+    </Box>
   );
 }
