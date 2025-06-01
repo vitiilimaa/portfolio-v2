394d547a2187576103cf25997a59afd1e0e07aa3
Vitor Batista <vitiilimaa@gmail.com>
2025-06-01 10:57:50 -0300

    fix: metadata no layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Montserrat } from "next/font/google";
 
 import "./globals.css";
 import "react-multi-carousel/lib/styles.css";
+import { Metadata } from "next";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -16,10 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-br" suppressHydrationWarning>
       <body className={`${montserrat.variable}`}>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Desenvolvedor Front-End - Vitor Batista",
+    description:
+      "Desenvolvedor com experiência, com objetivo em transformar ideias que fazem a diferença em linhas de código, criando soluções que impactam e melhoram a vida das pessoas.",
+  };
 }
